0d1f300985b439a16ab8cbc47a52eb36cc08abaa
André Luiz <silva.ala82@gmail.com>
2024-01-03 00:07:34 -0300

    fix: adjust env call at databse.ts

--- src/database.ts
+++ src/database.ts
@@ -7,7 +7,7 @@ export const config: Knex.Config = {
   connection:
     env.DATABASE_CLIENT === "sqlite"
       ? {
-          filename: env.DATABASE_URL,
+          filename: env.DATABASE_CLIENT,
         }
       : env.DATABASE_URL,
   useNullAsDefault: true,
